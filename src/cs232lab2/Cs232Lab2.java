d090503f3e8ce61dd7f62608e1c20f1d10ccf302
JnathanCarter <jcarter20@stu.jsu.edu>
2021-08-31 12:04:19 -0500

    Clean slate

--- src/cs232lab2/Cs232Lab2.java
+++ src/cs232lab2/Cs232Lab2.java
@@ -1,11 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
+
  */
 package cs232lab2;
 
-//import java.time.format.TextStyle;
 
 /**
  *
@@ -13,12 +11,12 @@ package cs232lab2;
  */
 public class Cs232Lab2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-       //this is a test
-        System.out.println("Hello world");
+       
+        
+
+
+
     }
     
 }
