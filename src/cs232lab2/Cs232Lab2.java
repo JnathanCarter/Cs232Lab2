74047d647f58c0838c8acd3f04d03d4848e0c843
JnathanCarter <jcarter20@stu.jsu.edu>
2021-09-01 13:56:26 -0500

    added loop that takes in file input but after taking it im getting error message

--- src/cs232lab2/Cs232Lab2.java
+++ src/cs232lab2/Cs232Lab2.java
@@ -8,25 +8,33 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
- *
+ * 
  * @author johnc
  */
 public class Cs232Lab2 {
-
+    int num_items = 0;
+  //  ArrayList<int> numbers = new ArrayList<int>();
     public static void main(String[] args) {
        try{
         File inputFile = new File("lab2_input-1.txt");
         Scanner sc = new Scanner(inputFile);
+        while(sc.hasNextLine()){
+            int temp =  sc.nextInt();
 
 
-        
+            System.out.println(temp);
+        }
+        sc.close();
 
        }
        catch(FileNotFoundException e){
             System.out.println("An error has occured");
             e.printStackTrace();
         }
-
+        /*
+        int temp =  sc.nextline;
+        System.out.println(temp);
+        */
 
 
     }
