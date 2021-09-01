fdc00da8e4a748b0cc1611d0a0f7c9060064498e
JnathanCarter <jcarter20@stu.jsu.edu>
2021-09-01 14:36:17 -0500

    more formatting

--- src/cs232lab2/Cs232Lab2.java
+++ src/cs232lab2/Cs232Lab2.java
@@ -17,36 +17,37 @@ public class Cs232Lab2 {
     int num_items = 0;
     static ArrayList<Integer> numbers = new ArrayList<Integer>();
     public static void main(String[] args) {
-       try{
-        File inputFile = new File("lab2_input-1.txt");
-        Scanner sc = new Scanner(inputFile);
-        while(sc.hasNextLine()){
-            int temp =  sc.nextInt();
-            numbers.add(temp);
-
-            //System.out.println(temp);
-            for (int i = 0 ; i < numbers.size(); i++){
-                System.out.println(numbers.get(i));
+        
+        /*
+        int temp =  sc.nextline;
+        System.out.println(temp);
+        */
+        
+        
+    }
+    public static void readFile(){
+        
+        try{
+            File inputFile = new File("lab2_input-1.txt");
+            Scanner sc = new Scanner(inputFile);
+            while(sc.hasNextLine()){
+                int temp =  sc.nextInt();
+                numbers.add(temp);
             }
+        
+            for (int i = 0 ; i < numbers.size(); i++){
+            System.out.println(numbers.get(i));               
+            }
+            sc.close();
+        //System.out.println(temp);
         }
-        sc.close();
-
-       }
-       catch(FileNotFoundException e){
+        catch(FileNotFoundException e){
             System.out.println("An error has occured");
             e.printStackTrace();
         }
         catch(NoSuchElementException e){
             //System.out.println("All of file read");
-        }
-
-        /*
-        int temp =  sc.nextline;
-        System.out.println(temp);
-        */
-
-
-    }
-    
+        }   
+    }   
 }
 
